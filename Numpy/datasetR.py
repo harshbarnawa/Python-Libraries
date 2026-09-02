d51d4a9ec96a7115f2614694230f1e5240f76340
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-09-02 12:40:40 +0530

    Did some advance operations on a random sales data for warmup and plotted a analytical graph using matplotlib

---
+++ Numpy/datasetR.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Data structure: [restuarant_id, 2021, 2022, 2023, 2024]
+
+sales_data = np.array([
+    [1, 150000, 180000, 220000, 250000], # Paradise Biryani
+    [1, 120000, 140000, 160000, 190000], # Beijing Bites
+    [1, 200000, 230000, 260000, 300000], # Pizza Hub
+    [1, 180000, 210000, 240000, 270000], # Burger Point
+    [1, 160000, 185000, 205000, 230000] # Chai Point
+])
+
