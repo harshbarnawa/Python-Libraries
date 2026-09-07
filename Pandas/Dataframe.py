a32307634f706ddded4a720a91c5674463c573fa
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-09-07 14:40:35 +0530

    dataframe basics done just syntax is quite complex but easy to understand the logic how it works

--- Pandas/Dataframe.py
+++ Pandas/Dataframe.py
@@ -6,6 +6,25 @@ import pandas as pd
 data = {"Name": ["Harsh", "Devraj", "Aditya"],
         "Age": [22, 20, 21]
         } ##here I am using dictionary
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, index = ["Student 1","Student 2","Student 3"])
 
 print(df)
+
+
+print(df.loc["Student 3"])
+print(df.iloc[1])
+
+## adding new column
+
+df["Hobbies"] = ["Games", "Songs", "Badminton"]
+
+print("\n",df)
+
+## adding new row
+
+new_row = pd.DataFrame([{"Name": "Bharat", "Age": 20, "Hobbies": "Movies"},
+                        {"Name": "Anurag", "Age": 21, "Hobbies": "Coding"}
+                        ], index = ["Student 4","Student 5"])
+
+df = pd.concat([df, new_row])
+print("\n",df)
