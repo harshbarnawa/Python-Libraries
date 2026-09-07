a89b5cc17db35a29835e89c48815c1d200e10785
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-09-07 14:20:30 +0530

    series completed

--- Pandas/Series.py
+++ Pandas/Series.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+## using dictionary
 cal = {"day 1": 1750,"day 2": 2100,"day 3": 3000}
 
 series = pd.Series(cal)
@@ -7,3 +7,6 @@ series = pd.Series(cal)
 print(series)
 series.loc["day 3"] += 100
 print(series.loc["day 3"])
+
+print(series[series >= 2000])
+print(series[series < 2000])
