57df8af8a642d859129560592034019de5267d2d
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-09-08 12:33:36 +0530

    revised the most important topic of pandas //aggregate functions best way to filter data

---
+++ Pandas/aggregate_fn.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+# aggregate function = Reduces a set of values into a single summary value
+#                      Used to summarize and analyze data
+#                      Often used with the groupby() function
+
+df = pd.read_csv("Pandas/data.csv")
+# whole dataframe
+print(df.mean(numeric_only=True)) # avg height weight and legendaries
+print(df.sum(numeric_only=True))
+print(df.min(numeric_only=True))
+print(df.max(numeric_only=True))
+print(df.count())
+
+print("\n")
+# single column
+
+print(df["Height"].mean()) 
+print(df["Height"].sum())
+print(df["Height"].min())
+print(df["Height"].max())
+print(df["Height"].count())
+
+print("\n")
+
+
+group = df.groupby("Type1")
+
+print(group["Height"].mean())
+print(group["Height"].sum())
+print(group["Height"].min())
+print(group["Height"].max())
+print(group["Height"].count())
